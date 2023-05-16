9e577faa8896911d668b929db3bc2551fa82f6e6
Katarina Virkes <katarinavirkes@gmail.com>
2023-05-16 21:58:53 +0200

    Adding cascade delete and routes

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import psycopg2
 import os
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 
 load_dotenv()
@@ -28,8 +28,201 @@ def getAllSlonista():
     result_dict = []
     for row in result:
         result_dict.extend(row)
-    print(result_dict)
     return make_response(jsonify(result_dict[0]), 200)
+
+
+@app.route('/skloniste', methods=['POST'])
+def createSkloniste():
+    request_data = request.get_json()
+
+    sql = f'''
+    INSERT INTO skloniste(id, naziv, adresa, id_mjesta)
+    VALUES({request_data["id"]}, '{request_data["naziv"]}',
+    '{request_data["adresa"]}', {request_data["id_mjesta"]})
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    return make_response(jsonify(""), 201)
+
+
+@app.route('/skloniste/<int:id_sklonista>')
+def getSkloniste(id_sklonista):
+    sql = f'''
+    SELECT row_to_json(r, true)
+    FROM ( SELECT
+    s.id, s.naziv, s.adresa, m.naziv as naziv_mjesta
+    FROM skloniste s
+    JOIN mjesto m
+    ON s.id_mjesta = m.id
+    WHERE m.id = {id_sklonista})
+    as r
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return make_response(jsonify(result), 200)
+
+
+@app.route('/skloniste/<int:id_sklonista>', methods=['PUT'])
+def updateSkloniste(id_sklonista):
+    request_data = request.get_json()
+
+    sql = f'''
+    UPDATE skloniste
+    SET naziv = '{request_data["naziv"]}',
+    adresa = '{request_data["adresa"]}',
+    id_mjesta = {request_data["id_mjesta"]}
+    WHERE id = {id_sklonista}
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    return make_response(jsonify(""), 200)
+
+
+@app.route('/skloniste/<int:id_sklonista>', methods=['DELETE'])
+def deleteSkloniste(id_sklonista):
+    sql = f'''
+    DELETE FROM skloniste
+    WHERE id = {id_sklonista}
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    return make_response(jsonify(""), 200)
+
+
+@app.route('/skloniste/<int:id_sklonista>/zivotinje')
+def getZivotinjeSklonista(id_sklonista):
+    sql = f'''
+    SELECT row_to_json(r, true)
+    FROM ( SELECT
+    z.broj_cipa, z.vrsta, z.datum_rodenja,
+    z.spol, z.pasmina, s.stanje
+    FROM zivotinja z
+    JOIN stanje_zivotinje s
+    ON z.id_stanja = s.id
+    WHERE z.id_sklonista = {id_sklonista})
+    as r
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    result_dict = []
+    for row in result:
+        result_dict.extend(row)
+    return make_response(jsonify(result_dict), 200)
+
+
+@app.route('/skloniste/<int:id_sklonista>/zivotinja/<int:broj_cipa>')
+def getZivotinja(id_sklonista, broj_cipa):
+    sql = f'''
+    SELECT row_to_json(r, true)
+    FROM ( SELECT
+    z.broj_cipa, z.vrsta, z.spol,
+    z.datum_rodenja,
+    z.pasmina, s.stanje
+    FROM zivotinja z
+    JOIN stanje_zivotinje s
+    ON z.id_stanja = s.id
+    WHERE z.id_sklonista = {id_sklonista}
+    AND z.broj_cipa = {broj_cipa})
+    as r
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return make_response(jsonify(result), 200)
+
+
+@app.route('/skloniste/<int:id_sklonista>/zivotinja', methods=['POST'])
+def createZivotinja(id_sklonista):
+    request_data = request.get_json()
+
+    sql = f'''
+    INSERT INTO zivotinja(broj_cipa, vrsta, spol, datum_rodenja,
+    pasmina, id_sklonista, id_stanja)
+    VALUES({request_data["broj_cipa"]}, '{request_data["vrsta"]}',
+    '{request_data["spol"]}', '{request_data["datum_rodenja"]}',
+    '{request_data["pasmina"]}', {id_sklonista}, {request_data["id_stanja"]})
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    return make_response(jsonify(""), 201)
+
+
+@app.route('/skloniste/<int:id_sklonista>/zivotinja/<int:broj_cipa>', methods=['PUT'])
+def updateZivotinja(id_sklonista, broj_cipa):
+    request_data = request.get_json()
+
+    sql = f'''
+    UPDATE zivotinja
+    SET vrsta = '{request_data["vrsta"]}',
+    spol = '{request_data["spol"]}',
+    datum_rodenja = '{request_data["datum_rodenja"]}',
+    pasmina = '{request_data["pasmina"]}',
+    id_stanja = {request_data["id_stanja"]}
+    WHERE broj_cipa = {broj_cipa}
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    return make_response(jsonify(""), 200)
+
+
+@app.route('/skloniste/<int:id_sklonista>/zivotinja/<int:broj_cipa>', methods=['DELETE'])
+def deleteZivotinja(id_sklonista, broj_cipa):
+    sql = f'''
+    DELETE FROM zivotinja
+    WHERE broj_cipa = {broj_cipa}
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    return make_response(jsonify(""), 200)
+
+
+@app.route('/mjesta')
+def getAllMjesta():
+    sql = '''
+    SELECT json_agg(mjesto)::jsonb FROM mjesto
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    result_dict = []
+    for row in result:
+        result_dict.extend(row)
+    return make_response(jsonify(result_dict[0]), 200)
+
+
+@app.route('/mjesto/<int:id_mjesta>')
+def getMjesto(id_mjesta):
+    sql = f'''
+    SELECT json_agg(mjesto)::jsonb FROM mjesto
+    WHERE id = {id_mjesta}
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return make_response(jsonify(result), 200)
+
+
+@app.route('/stanja')
+def getAllStanja():
+    sql = '''
+    SELECT json_agg(stanje_zivotinje)::jsonb FROM stanje_zivotinje
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    result_dict = []
+    for row in result:
+        result_dict.extend(row)
+    return make_response(jsonify(result_dict[0]), 200)
+
+
+@app.route('/stanje/<int:id_stanja>')
+def getStanje(id_stanja):
+    sql = f'''
+    SELECT json_agg(stanje_zivotinje)::jsonb FROM stanje_zivotinje
+    WHERE id = {id_stanja}
+    '''
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return make_response(jsonify(result), 200)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
