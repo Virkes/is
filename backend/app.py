d17c44a5033ac8f7cd45e174466e5e0036454f07
Katarina Virkes <katarinavirkes@gmail.com>
2023-05-17 20:00:55 +0200

    Minor changes

--- backend/app.py
+++ backend/app.py
@@ -21,14 +21,20 @@ cursor = conn.cursor()
 @app.route('/')
 def getAllSlonista():
     sql = '''
-    SELECT json_agg(skloniste)::jsonb FROM skloniste
+    SELECT row_to_json(r, true)
+    FROM ( SELECT
+    s.id, s.naziv, s.adresa, s.id_mjesta, m.naziv as naziv_mjesta
+    FROM skloniste s
+    JOIN mjesto m
+    ON s.id_mjesta = m.id)
+    as r
     '''
     cursor.execute(sql)
     result = cursor.fetchall()
     result_dict = []
     for row in result:
         result_dict.extend(row)
-    return make_response(jsonify(result_dict[0]), 200)
+    return make_response(jsonify(result_dict), 200)
 
 
 @app.route('/skloniste', methods=['POST'])
@@ -36,8 +42,8 @@ def createSkloniste():
     request_data = request.get_json()
 
     sql = f'''
-    INSERT INTO skloniste(id, naziv, adresa, id_mjesta)
-    VALUES({request_data["id"]}, '{request_data["naziv"]}',
+    INSERT INTO skloniste(naziv, adresa, id_mjesta)
+    VALUES('{request_data["naziv"]}',
     '{request_data["adresa"]}', {request_data["id_mjesta"]})
     '''
     cursor.execute(sql)
@@ -50,11 +56,11 @@ def getSkloniste(id_sklonista):
     sql = f'''
     SELECT row_to_json(r, true)
     FROM ( SELECT
-    s.id, s.naziv, s.adresa, m.naziv as naziv_mjesta
+    s.id, s.naziv, s.adresa, s.id_mjesta, m.naziv as naziv_mjesta
     FROM skloniste s
     JOIN mjesto m
     ON s.id_mjesta = m.id
-    WHERE m.id = {id_sklonista})
+    WHERE s.id = {id_sklonista})
     as r
     '''
     cursor.execute(sql)
@@ -95,7 +101,7 @@ def getZivotinjeSklonista(id_sklonista):
     SELECT row_to_json(r, true)
     FROM ( SELECT
     z.broj_cipa, z.vrsta, z.datum_rodenja,
-    z.spol, z.pasmina, s.stanje
+    z.spol, z.pasmina, z.id_stanja, s.stanje
     FROM zivotinja z
     JOIN stanje_zivotinje s
     ON z.id_stanja = s.id
@@ -110,19 +116,18 @@ def getZivotinjeSklonista(id_sklonista):
     return make_response(jsonify(result_dict), 200)
 
 
-@app.route('/skloniste/<int:id_sklonista>/zivotinja/<int:broj_cipa>')
-def getZivotinja(id_sklonista, broj_cipa):
+@app.route('/zivotinja/<int:broj_cipa>')
+def getZivotinja(broj_cipa):
     sql = f'''
     SELECT row_to_json(r, true)
     FROM ( SELECT
     z.broj_cipa, z.vrsta, z.spol,
     z.datum_rodenja,
-    z.pasmina, s.stanje
+    z.pasmina, z.id_stanja, s.stanje
     FROM zivotinja z
     JOIN stanje_zivotinje s
     ON z.id_stanja = s.id
-    WHERE z.id_sklonista = {id_sklonista}
-    AND z.broj_cipa = {broj_cipa})
+    WHERE z.broj_cipa = {broj_cipa})
     as r
     '''
     cursor.execute(sql)
@@ -146,8 +151,8 @@ def createZivotinja(id_sklonista):
     return make_response(jsonify(""), 201)
 
 
-@app.route('/skloniste/<int:id_sklonista>/zivotinja/<int:broj_cipa>', methods=['PUT'])
-def updateZivotinja(id_sklonista, broj_cipa):
+@app.route('/zivotinja/<int:broj_cipa>', methods=['PUT'])
+def updateZivotinja(broj_cipa):
     request_data = request.get_json()
 
     sql = f'''
@@ -164,8 +169,8 @@ def updateZivotinja(id_sklonista, broj_cipa):
     return make_response(jsonify(""), 200)
 
 
-@app.route('/skloniste/<int:id_sklonista>/zivotinja/<int:broj_cipa>', methods=['DELETE'])
-def deleteZivotinja(id_sklonista, broj_cipa):
+@app.route('/zivotinja/<int:broj_cipa>', methods=['DELETE'])
+def deleteZivotinja(broj_cipa):
     sql = f'''
     DELETE FROM zivotinja
     WHERE broj_cipa = {broj_cipa}
